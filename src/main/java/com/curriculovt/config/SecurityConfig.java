2f46a8a0c70856f64888022bdb4f41a251f3d79c
thalisson <thalissonmoura138@gmail.com>
2026-02-28 12:35:29 -0300

    :sparkles:feat: coloca rota do servidor teste

--- src/main/java/com/curriculovt/config/SecurityConfig.java
+++ src/main/java/com/curriculovt/config/SecurityConfig.java
@@ -59,7 +59,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://31.97.22.121:5173"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
