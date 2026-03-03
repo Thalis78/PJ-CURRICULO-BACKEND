a6b10025502ec04ecb605ce70af381082af43cc3
thalisson <thalissonmoura138@gmail.com>
2026-03-03 19:26:31 -0300

    :bug:fix: removendo prefixo /api duplicado e ajustando ordem do SecurityConfig

--- src/main/java/com/curriculovt/controllers/AuthController.java
+++ src/main/java/com/curriculovt/controllers/AuthController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
